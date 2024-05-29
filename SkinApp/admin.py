50cb8d8ca26eea2b5c4138723f6688ff9b9717ac
Mohammed Alsumary <139048440+Alsumary@users.noreply.github.com>
2024-05-29 23:13:24 +0300

    Update admin.py

--- SkinApp/admin.py
+++ SkinApp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import home, about, OurTeam, teamMemebers, Treatments, clinicInfo, Appointment
+from .models import home, about, OurTeam, teamMemebers, Treatments, clinicInfo, Appointment, typeApp
 
 # Register your models here.
 admin.site.register(home)
@@ -9,3 +9,4 @@ admin.site.register(Treatments)
 admin.site.register(teamMemebers)
 admin.site.register(clinicInfo)
 admin.site.register(Appointment)
+admin.site.register(typeApp)
